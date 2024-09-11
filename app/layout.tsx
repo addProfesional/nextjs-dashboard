0d3ffbc5ffd185e89673a95916f87c34185b19e7
Adrian Ortega <adrian.ortega@t1paginas.com>
2024-09-11 16:34:44 -0600

    Added basic route and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.className}`}>
         {children}
+        <footer className="flex justify-center items-center h-20 bg-gray-800 text-white">
+          Hecho con ❤️ por Vercel
+        </footer>
       </body>
     </html>
   );
