8951f59874083aa11f0e29b809f459f25b43e0ce
Adrian Ortega <adrian.ortega@t1paginas.com>
2024-09-11 16:10:30 -0600

    Added a secondary font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Montserrat, Lusitana } from 'next/font/google'
+import { Montserrat, Lusitana, Inter } from 'next/font/google'
 
 export const montserrat = Montserrat({ subsets: ['latin'] })
 export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
+export const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] })
